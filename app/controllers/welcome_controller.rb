e75e1e9137042f644e1ca7355530bb1a99dd73ae
NelcifranMagalhaes <nelcifranpires@gmail.com>
2017-06-25 18:40:22 -0300

    welcome page created and routes config

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,13 @@
+class WelcomeController < ApplicationController
+  # GET /vendors
+  # GET /vendors.json
+  def index
+    
+  end
+
+  # GET /vendors/1
+  # GET /vendors/1.json
+  def show
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :goals
   resources :vendors
   resources :stores
-  root "stores#index"
+  root "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
